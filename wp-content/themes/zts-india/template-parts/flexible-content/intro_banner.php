54a51a3371def33f719edc748c148379682c6a91
Sujan nagaju <nagajus@gmail.com>
2022-06-12 22:14:04 +0545

    services category listing page and css push

--- wp-content/themes/zts-india/template-parts/flexible-content/intro_banner.php
+++ wp-content/themes/zts-india/template-parts/flexible-content/intro_banner.php
@@ -8,32 +8,37 @@ $banner_media      = get_sub_field( 'banner_media' );
 $banner_cta_button = get_sub_field( 'banner_cta_button' );
 
 ?>
-<div class="intro-banner">
-    <?php if ( $banner_intro_text || $banner_cta_button ) : ?>
-        <div class="banner-intro">
-            <?php
-                // Banner Intro Text.
-            if ( $banner_intro_text ) {
-                echo $banner_intro_text;
-            }
-            ?>
+<div class="service-intro-banner">
+    <div class="container">
+        <div class="intro-wrapper">
+            <?php if ( $banner_intro_text || $banner_cta_button ) : ?>
+                <div class="intro-content">
+                    <div class="content-inner">
+                        <?php
+                            // Banner Intro Text.
+                        if ( $banner_intro_text ) {
+                            echo $banner_intro_text;
+                        }
+                        ?>
+                        <?php
+                            // Banner CTA Button.
+                        if ( $banner_cta_button ) {
+                            ?>
+                            <div class="button-wrap">
+                                <a href="<?php echo ( $banner_cta_button['url'] ) ? $banner_cta_button['url'] : '#'; ?>" class="btn btn-red-outline btn-2x" target="<?php echo $banner_cta_button['target']; ?>">
+                                    <?php echo $banner_cta_button['title']; ?>
+                                </a>
+                            </div>
+                        <?php } ?>
+                    </div><!-- content-inner -->
+                </div><!-- intro-content -->
+            <?php endif; ?>
 
-            <?php
-                // Banner CTA Button.
-            if ( $banner_cta_button ) {
-                ?>
-                <div class="button-wrap">
-                    <a href="<?php echo ( $banner_cta_button['url'] ) ? $banner_cta_button['url'] : '#'; ?>" class="btn btn-red-outline" target="<?php echo $banner_cta_button['target']; ?>">
-                        <?php echo $banner_cta_button['title']; ?>
-                    </a>
-                </div>
+            <?php if ( $banner_media ) { ?>
+                <div class="intro-media">
+                    <?php echo $banner_media; ?>
+                </div><!-- intro-media -->
             <?php } ?>
-        </div><!-- banner-intro -->
-    <?php endif; ?>
-
-    <?php if ( $banner_media ) { ?>
-        <div class="banner-media">
-            <?php echo $banner_media; ?>
-        </div><!-- banner-media -->
-    <?php } ?>
-</div><!-- intro-banner -->
+        </div><!-- intro-wrapper -->
+    </div><!-- container -->
+</div><!-- service-intro-banner -->
--- wp-content/themes/zts-india/template-parts/flexible-content/service_categories_listing.php
+++ wp-content/themes/zts-india/template-parts/flexible-content/service_categories_listing.php
@@ -10,63 +10,79 @@ $service_categories = get_terms(
 		'hide_empty' => $hide_empty,
 	)
 );
-if ( $service_categories ) :
-	foreach ( $service_categories as $service_category ) {
-		$term_id = $service_category->term_id;
-		$args = array(
-			'post_type'      => 'services',
-			'posts_per_page' => -1,
-			'post_status'    => 'publish',
-			'fields'         => 'ids',
-			'tax_query'      => array(
-				'taxonomy' => 'service-categories',
-				'field'    => 'term_id',
-				'terms'    => $term_id,
-			),
-		);
-
-		$services_post = get_posts( $args );
-
-		// List Service categories and related posts.
-		?>
-		<div class="category-listing">
+?>
+<section class="service-cat-section">
+	<div class="container">
+		<div class="service-cat-wrap">
 			<?php
-			// Term Image.
-			$category_image              = get_field( 'category_image', $service_category );
-			$category_custom_description = get_field( 'service_category_custom_description', $service_category );
-			if ( $category_image ) {
-				?>
-				<div class="category-image">
-					<?php echo wp_get_attachment_image( $category_image['ID'], 'full' ); ?>
-				</div>
-			<?php } ?>
+			if ( $service_categories ) :
+				foreach ( $service_categories as $service_category ) {
+					$term_id = $service_category->term_id;
+					$args = array(
+						'post_type'      => 'services',
+						'posts_per_page' => -1,
+						'post_status'    => 'publish',
+						'fields'         => 'ids',
+						'tax_query'      => array(
+							array(
+							'taxonomy' => 'service-categories',
+							'field'    => 'term_id',
+							'terms'    => $term_id,
+							)
+						),
+					);
 
-			<div class="term-details-wrapper">
-				<h2><?php echo $service_category->name; ?></h2>
-				<?php
-				if ( $category_custom_description ) :
-					echo $category_custom_description;
-				endif;
-				?>
+					$services_post = get_posts( $args );
 
-				<?php
-					// Services post listing.
-				if ( $services_post ) {
-					echo '<a href="' . esc_url( home_url() . '/services/' ) . '">' . __( 'Know in detail', 'zts-india' ) . '</a>';
-					echo '<ul>';
-					foreach ( $services_post as $service ) {
-						echo '<li>';
-						echo '<a href="' . get_the_permalink( $service ) . '">' . get_the_title( $service ) . '</a>';
-						echo '</li>';
-					}
-					echo '</ul>';
-				}
-				?>
+					// List Service categories and related posts.
+					?>
+					<div class="category-listing">
+						<div class="category-inner">
+						<?php
+							// Term Image.
+							$category_image              = get_field( 'category_image', $service_category );
+							$category_custom_description = get_field( 'service_category_custom_description', $service_category );
+							if ( $category_image ) {
+								?>
+								<div class="category-image">
+									<?php echo wp_get_attachment_image( $category_image['ID'], 'full' ); ?>
+								</div>
+							<?php } ?>
 
-			</div><!-- term-details-wrapper -->
-		</div><!-- category-listing -->
-		<?php
+							<div class="category-info">
+								<h3><?php echo $service_category->name; ?></h3>
+								<?php
+								if ( $category_custom_description ) :
+									echo $category_custom_description;
+								endif;
+								?>
+							</div><!-- category-info -->
+							<div class="service-cat-listing">
+								<div class="service-list-inner">
+									<h5><?php _e('Service includes:', 'zts-india') ?></h5>
+									<?php
+									// Services post listing.
+									if ( $services_post ) {
+										//echo '<a href="' . esc_url( home_url() . '/services/' ) . '">' . __( 'Know in detail', 'zts-india' ) . '</a>';
+										echo '<ul>';
+										foreach ( $services_post as $service ) {
+											echo '<li>';
+											echo '<a href="' . get_the_permalink( $service ) . '">' . get_the_title( $service ) . '</a>';
+											echo '</li>';
+										}
+										echo '</ul>';
+									}
+									?>
+								</div>
+							</div><!-- service-cat-listing -->
+						</div><!-- category-inner -->
+					</div><!-- category-listing -->
+					<?php
 
-	} // End foreach loop.
+				} // End foreach loop.
 
-endif;
+			endif;
+			?>
+		</div><!-- service-cat-wrap -->
+	</div><!-- container -->
+</section><!-- service-cat-section -->
