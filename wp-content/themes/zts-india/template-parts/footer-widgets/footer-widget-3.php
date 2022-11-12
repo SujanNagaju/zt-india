94f0fc34df98d5ec7e8a8b18d70752b7275ba95d
Sujan nagaju <nagajus@gmail.com>
2022-11-12 11:15:43 +0545

    Quick links section

--- wp-content/themes/zts-india/template-parts/footer-widgets/footer-widget-3.php
+++ wp-content/themes/zts-india/template-parts/footer-widgets/footer-widget-3.php
@@ -5,51 +5,29 @@
 
 $widget_3_title    = get_field( 'widget_3_title', 'option' );
 $widget_3_settings = get_field( 'widget_3_settings', 'option' );
-$posts_per_page    = $widget_3_settings['posts_per_page'];
-$posts_per_page    = $posts_per_page ? $posts_per_page : 3;
+$quick_links = $widget_3_settings['quick_links'];
+?>
+<div class="col-lg-2 col-md-6">
+    <div class="footer-block">
+    <?php echo '<h5>Recent Posts</h5>'; ?>
+    <?php if( $quick_links ): ?>
+        <div class="links">
+            <ul>
+                <?php foreach( $quick_links as $link ): ?>
+                    <li><a href="<?php echo get_the_permalink( $link )?>"><?php echo get_the_title( $link ); ?></a></li>
+                <?php endforeach;?>
 
-// Query Recent posts.
-// $recent_posts = new WP_Query(
-// 	array(
-// 		'post_type'      => 'post',
-// 		'post_status'    => 'publish',
-// 		'posts_per_page' => $posts_per_page,
-// 	)
-// );
-
-// if ( $recent_posts->have_posts() ) :
-	?>
-    <div class="col-lg-2 col-md-6">
-        <div class="footer-block">
-        <?php echo '<h5>Recent Posts</h5>'; ?>
-            <div class="links">
-                <ul>
-                    <li><a href="<?php echo home_url(); ?>/blog">Blog</a></li>
-                    <li><a href="<?php echo home_url(); ?>/">Career</a></li>
-                    <li><a href="<?php echo home_url(); ?>/portfolios/">Case Study</a></li>
-                    <li><a href="<?php echo home_url(); ?>/contact-us/">Contact Us</a></li>
-                    <li><a href="<?php echo home_url(); ?>/">Sitemap</a></li>
-                    <li><a href="<?php echo home_url(); ?>/">Privacy Policy</a></li>
-                    <li><a href="<?php echo home_url(); ?>/">Terms of Use</a></li>
-                <?php
-                    // while ( $recent_posts->have_posts() ) :
-                    //     $recent_posts->the_post();
-                    //     ?>
-                          <!-- <li> -->
-                              <!-- <a href="<?php //echo the_permalink(); ?>"> -->
-                                  <!-- <div class="desc"> -->
-                                      <!-- <h6><?php //the_title(); ?></h6> -->
-                                      <!-- <span><?php //echo get_the_date(''); ?></span> -->
-                                  <!-- </div> -->
-                              <!-- </a> -->
-                          <!-- </li> -->
-                          <?php
-                    // endwhile;
-                    // wp_reset_postdata();
-                    ?>
-                </ul>
-            </div><!-- recent-posts -->
-        </div><!-- footer-block -->
-    </div><!-- col-lg-4 -->
-	<?php
-// endif;
+                <!-- <li><a href="<?php echo home_url(); ?>/blog">Blog</a></li>
+                <li><a href="<?php echo home_url(); ?>/">Career</a></li>
+                <li><a href="<?php echo home_url(); ?>/portfolios/">Case Study</a></li>
+                <li><a href="<?php echo home_url(); ?>/contact-us/">Contact Us</a></li>
+                <li><a href="<?php echo home_url(); ?>/">Sitemap</a></li>
+                <li><a href="<?php echo home_url(); ?>/">Privacy Policy</a></li>
+                <li><a href="<?php echo home_url(); ?>/">Terms of Use</a></li> -->
+            
+            </ul>
+        </div><!-- links -->
+    <?php endif; ?>
+    </div><!-- footer-block -->
+</div><!-- col-lg-4 -->
+<?php
